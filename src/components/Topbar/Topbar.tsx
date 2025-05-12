e326bfe9a2d33222a76d9d586b031feca9a893f3
ZuchiSpeed <zuchi737@gmail.com>
2025-05-12 14:52:06 +0200

    video model

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,12 +1,16 @@
+import { auth } from "@/firebase/firebase";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type TopbarProps = {
   problemPage?: boolean;
 };
 
 const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
+  const [user] = useAuthState(auth);
+
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div className={`flex w-full items-center justify-between`}>
@@ -23,11 +27,31 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
               Premuim
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded">
-              Sign In
-            </button>
-          </Link>
+          {!user && (
+            <Link href="/auth">
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded">
+                Sign In
+              </button>
+            </Link>
+          )}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <Image
+                src="/avatar.png"
+                alt="Avatar"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+              z-40 group-hover:scale-100 scale-0 
+              transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
--- src/components/problemsTable/problemsTable.tsx
+++ src/components/problemsTable/problemsTable.tsx
@@ -1,56 +1,114 @@
 import { problems } from "@/mockProblems/problems";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsCheckCircle } from "react-icons/bs";
 import { AiFillYoutube } from "react-icons/ai";
+import YouTube from "react-youtube";
+import { IoClose } from "react-icons/io5";
 
 type ProblemsTableProps = {};
 
 const ProblemsTable: React.FC<ProblemsTableProps> = () => {
-  return (
-    <tbody className="text-body">
-      {problems.map((problem, idx) => {
-        const difficulyColor =
-          problem.difficulty === "Easy"
-            ? "text-dark-green-s"
-            : problem.difficulty === "Medium"
-            ? "text-dark-yellow"
-            : "text-dark-pink";
+  //state to open youtube player
+  const [youtubePlayer, setYoutubePlayer] = useState({
+    isOpen: false,
+    videoId: "",
+  });
 
-        return (
-          <tr
-            className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
-            key={problem.id}
-          >
-            <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
-              <BsCheckCircle fontSize={"18"} width="18" />
-            </th>
-            <td className="px-6 py-4">
-              <Link
-                className="hover:text-blue-600 cursor-pointer"
-                href={`/problems/${problem.id}`}
-              >
-                {problem.title}
-              </Link>
-            </td>
-            <td className={`px-6 py-4 ${difficulyColor}`}>
-              {problem.difficulty}
-            </td>
-            <td className={"px-6 py-4"}>{problem.category}</td>
-            <td className={"px-6 py-4"}>
-              {problem.videoId ? (
-                <AiFillYoutube
-                  fontSize={"18"}
-                  className="hover:text-red-600 cursor-pointer"
+  // Function to close the modal and reset videoId
+  // This function is called when the user clicks outside the modal or presses Escape
+  const closeModel = () => {
+    setYoutubePlayer((prev) => ({ ...prev, isOpen: false, videoId: "" }));
+  };
+
+  // Listens for Escape key press to close modal
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") closeModel();
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []); // Empty dependency array ensures effect runs only once
+
+  return (
+    <>
+      <tbody className="text-body">
+        {problems.map((problem, idx) => {
+          const difficulyColor =
+            problem.difficulty === "Easy"
+              ? "text-dark-green-s"
+              : problem.difficulty === "Medium"
+              ? "text-dark-yellow"
+              : "text-dark-pink";
+
+          return (
+            <tr
+              className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
+              key={problem.id}
+            >
+              <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
+                <BsCheckCircle fontSize={"18"} width="18" />
+              </th>
+              <td className="px-6 py-4">
+                <Link
+                  className="hover:text-blue-600 cursor-pointer"
+                  href={`/problems/${problem.id}`}
+                >
+                  {problem.title}
+                </Link>
+              </td>
+              <td className={`px-6 py-4 ${difficulyColor}`}>
+                {problem.difficulty}
+              </td>
+              <td className={"px-6 py-4"}>{problem.category}</td>
+              <td className={"px-6 py-4"}>
+                {problem.videoId ? (
+                  <AiFillYoutube
+                    fontSize={"18"}
+                    className="hover:text-red-600 cursor-pointer"
+                    //onClick sets the youtube player to open
+                    //and sets the videoId to the problem's videoId
+                    onClick={() => {
+                      setYoutubePlayer({
+                        isOpen: true,
+                        videoId: problem.videoId as string,
+                      });
+                    }}
+                  />
+                ) : (
+                  <p className="text-gray-400">Coming Soon</p>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+      {/* Youtube player modal */}
+      {youtubePlayer.isOpen && (
+        <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center">
+          <div
+            className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"
+            onClick={closeModel} // Clicking the backdrop triggers modal close
+          ></div>
+          <div className="w-full z-50 h-full px-6 relative max-w-4xl">
+            <div className="w-full h-full flex items-center justify-center relative">
+              <div className="w-full relative">
+                <IoClose
+                  fontSize={"35"}
+                  className="cursor-pointer absolute -top-16 right-0"
+                  onClick={closeModel} // Clicking the close button triggers modal close
                 />
-              ) : (
-                <p className="text-gray-400">Coming Soon</p>
-              )}
-            </td>
-          </tr>
-        );
-      })}
-    </tbody>
+                <YouTube
+                  videoId={youtubePlayer.videoId} //dynamic videoId data coming from state
+                  loading="lazy"
+                  iframeClassName="w-full min-h-[500px]"
+                />
+              </div>
+            </div>
+          </div>
+        </tfoot>
+      )}
+    </>
   );
 };
 
